ee991f7f1a48aadc3bf35966cd121beaac4abe10
Mitchell Gorham <s2837308@student.ad.griffith.edu.au>
2019-09-24 22:40:33 +1000

    Prepared for the addition of additional game modes

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,4 +1,4 @@
-import tkinter 
+import tkinter as tk 
 import tkinter.messagebox as msgbox
 import tkinter.simpledialog as dialog
 
@@ -8,42 +8,51 @@ import random
 
 import time
 import threading
-
-# Constant Init
-
-window = tkinter.Tk()
+# # # # # # # # #
+# Constant Init #
+# # # # # # # # #
+window = tk.Tk()
 window.title("Minesweeper")
+window.iconbitmap("game.ico")
 
 flagGraphic = "⛳"  # Image that is displayed on Flag
 mineGraphic = "⚙"   # Image that is displayed on Mine
+resetGraphic = "☺"   # Image that is displayed on Reset Button
 # Colour of Numbers indicating mine presence (Values from Minesweeper)
 colour = ['#FFFFFF', '#0000FF', '#008200', '#FF0000', '#000084', '#840000', '#008284', '#840084', '#000000']
 
-#Variable Init
-
+# # # # # # # # #
+# Variable Init #
+# # # # # # # # #
 rows = 9            # Default rows on first run
 columns = 9         # Default columns on first run
 mineCount = 10      # Default mines on first run
 
 # Amount of flags set to mine count, assigned to tk.label that displays it
 flagCount = mineCount                 
-flagsLabel = tkinter.StringVar()
+flagsLabel = tk.StringVar()
 flagsLabel.set(flagCount)
 
 # Time elapsed since first move, assigned to tk.label that displays it
 gameTime = 0
-gameTimeLabel = tkinter.StringVar()
+gameTimeLabel = tk.StringVar()
 gameTimeLabel.set(gameTime)
+
+# Set up reset graphic
+resetLabel = tk.StringVar()
+resetLabel.set(resetGraphic)
 
 firstMove = True    # Tracks when user takes first move
 gameOver = False    # Tracks when a mine is hit or game is won
 
-gameField = []
-buttons = []
-customGameSizes = []
+gameField = []          # Positioning of cells
+buttons = []            # Tracks each cell
+customGameSizes = []    # Stores players custom game stats
 
-# Function Declarations
-# x = rows, y = columns when looping
+# # # # # # # # # # # # #
+# Function Declarations #
+# x = rows, y = columns #
+# # # # # # # # # # # # #
 
 def loadConfig():
     global rows, columns, mineCount, customGameSizes
@@ -78,32 +87,32 @@ def saveConfig():
         config.write(file)
 
 def createMenu():
-    menubar = tkinter.Menu(window)
+    menuBar = tk.Menu(window)
 
     # Size Menu
-    menusize = tkinter.Menu(window, tearoff=0)
-    menusize.add_command(label="Beginner (9x9 with 10 mineCount)", command=lambda: setSize(9, 9, 10))
-    menusize.add_command(label="Intermediate (16x16 with 40 mineCount)", command=lambda: setSize(16, 16, 40))
-    menusize.add_command(label="Expert (16x30 with 99 mineCount)", command=lambda: setSize(16, 30, 99))
-    menusize.add_separator()
-    menusize.add_command(label="Custom", command=setCustomSize)
+    sizeMenu = tk.Menu(window, tearoff=0)
+    sizeMenu.add_command(label="Beginner (9x9 with 10 mineCount)", command=lambda: setSize(9, 9, 10))
+    sizeMenu.add_command(label="Intermediate (16x16 with 40 mineCount)", command=lambda: setSize(16, 16, 40))
+    sizeMenu.add_command(label="Expert (16x30 with 99 mineCount)", command=lambda: setSize(16, 30, 99))
+    sizeMenu.add_separator()
+    sizeMenu.add_command(label="Custom", command=setCustomSize)
     if len(customGameSizes) > 0:
-        menusize.add_separator()
+        sizeMenu.add_separator()
         for x in range(0, len(customGameSizes)):
-            menusize.add_command(label=str(customGameSizes[x][0])+"x"+str(customGameSizes[x][1])+" with "+str(customGameSizes[x][2])+" mineCount", command=lambda customGameSizes=customGameSizes: setSize(customGameSizes[x][0], customGameSizes[x][1], customGameSizes[x][2]))
-    menubar.add_cascade(label="Options", menu=menusize)
+            sizeMenu.add_command(label=str(customGameSizes[x][0])+"x"+str(customGameSizes[x][1])+" with "+str(customGameSizes[x][2])+" mineCount", command=lambda customGameSizes=customGameSizes: setSize(customGameSizes[x][0], customGameSizes[x][1], customGameSizes[x][2]))
+    menuBar.add_cascade(label="Options", menu=sizeMenu)
 
     # Game type Menu
-    gameTypes = tkinter.Menu(window, tearoff=0)
+    gameTypes = tk.Menu(window, tearoff=0)
     gameTypes.add_command(label="Default")
     gameTypes.add_command(label="Hexes")
     gameTypes.add_command(label="Linemap")
-    menubar.add_cascade(label="Game Type", menu=gameTypes)
+    menuBar.add_cascade(label="Game Type", menu=gameTypes)
 
-    # Exit Command (Find a way to stick to the Right side)
-    menubar.add_command(label="Exit", command=lambda: window.destroy())
+    # Exit Command
+    menuBar.add_command(label="Exit", command=lambda: window.destroy())
     
-    window.config(menu=menubar)
+    window.config(menu=menuBar)
 
 def setCustomSize():
     global customGameSizes
@@ -188,18 +197,18 @@ def prepareWindow():
     global rows, columns, buttons
 
     # Create header window
-    tkinter.Label(window, textvariable=flagsLabel).grid(row=0, column=0, columnspan=3, sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
-    tkinter.Button(window, text="☺", command=gameRestart).grid(row=0, column=3, columnspan=columns-6, sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
-    tkinter.Label(window, textvariable=gameTimeLabel).grid(row=0, column=columns-3, columnspan=3, sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
+    tk.Label(window, textvariable=flagsLabel).grid(row=0, column=0, columnspan=3, sticky=tk.N+tk.W+tk.S+tk.E)
+    tk.Button(window, textvariable=resetLabel, command=gameRestart).grid(row=0, column=3, columnspan=columns-6, sticky=tk.N+tk.W+tk.S+tk.E)
+    tk.Label(window, textvariable=gameTimeLabel).grid(row=0, column=columns-3, columnspan=3, sticky=tk.N+tk.W+tk.S+tk.E)
     
     # Create and bind event to mouse buttons for each button on the game field
     buttons = []
     for x in range(0, rows):
         buttons.append([])
         for y in range(0, columns):
-            b = tkinter.Button(window, text=" ", width=2, command=lambda x=x,y=y: revealCell(x,y))
+            b = tk.Button(window, text=" ", width=2, command=lambda x=x,y=y: revealCell(x,y))
             b.bind("<Button-3>", lambda e, x=x, y=y:flagCell(x, y))
-            b.grid(row=x+1, column=y, sticky=tkinter.N+tkinter.W+tkinter.S+tkinter.E)
+            b.grid(row=x+1, column=y, sticky=tk.N+tk.W+tk.S+tk.E)
             buttons[x].append(b)
 
 def gameRestart():
@@ -212,7 +221,7 @@ def gameRestart():
     gameTimeLabel.set(gameTime)
     # Destroy all instances of info windows
     for win in window.winfo_children():
-        if type(win) != tkinter.Menu:
+        if type(win) != tk.Menu:
             win.destroy()
     # Reinitialise thread and run prepare functions
     gameTimeThread = threading.Timer(1.0, gameTimer)
@@ -235,7 +244,7 @@ def revealCell(x,y):
         buttons[x][y]["text"] = mineGraphic
         buttons[x][y].config(background='red', disabledforeground='black')
         gameOver = True
-        #tkinter.messagebox.showinfo("Lose Message")
+        #tk.messagebox.showinfo("Lose Message")
         revealMines(x,y,rows,columns)
 
     else:   # otherwise gameField contains
@@ -246,7 +255,7 @@ def revealCell(x,y):
         cascadeCell(x,y)
 
     buttons[x][y]['state'] = 'disabled'
-    buttons[x][y].config(relief=tkinter.SUNKEN)
+    buttons[x][y].config(relief=tk.SUNKEN)
     # Check if player has won
     win = True
     for x in range(0, rows):
@@ -254,7 +263,7 @@ def revealCell(x,y):
             if gameField[x][y] != -1 and buttons[x][y]["state"] == "normal":
                 win = False
     if win and gameOver == False:
-        #tkinter.messagebox.showinfo("Win Message")
+        #tk.messagebox.showinfo("Win Message")
         gameOver = True
         revealMines(x,y,rows,columns)
 
@@ -270,7 +279,7 @@ def cascadeCell(x,y):
     else:   # Cell has no adjacent mines
         buttons[x][y]["text"] = " "
     buttons[x][y].config(disabledforeground=colour[gameField[x][y]])
-    buttons[x][y].config(relief=tkinter.SUNKEN)
+    buttons[x][y].config(relief=tk.SUNKEN)
     buttons[x][y]['state'] = 'disabled'
     # Check each adjacent cell
     if gameField[x][y] == 0:
@@ -335,6 +344,9 @@ def gameTimer():
         gameTimeLabel.set(gameTime)
         time.sleep(1)        
 
+# # # # # #
+# M A I N #
+# # # # # #
 if __name__ == "__main__":
     # Check to see if config data already exists to load from
     if os.path.exists("config.ini"):
